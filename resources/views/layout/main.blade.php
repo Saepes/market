8c41489b292cdd1be942f4223ed7a10dd801140c
Saepes <tima123tima1@gmail.com>
2021-05-04 09:42:31 +0300

    Display fix slider

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @yield('custom_css')
     @yield('custom_css_head')
 </head>
