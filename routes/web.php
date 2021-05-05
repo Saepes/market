45e760f62ce9d6e890e8e1cf57ee843921ad245f
Saepes <tima123tima1@gmail.com>
2021-05-05 11:38:37 +0300

    Adding Interface Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,21 +12,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route main page
 Route::get('/', 'HomePageController@showIndex') -> name('home');
 Route::get('/category', 'CategoryController@showCategory') -> name('category');
 Route::get('/cart', 'CartController@showCart') -> name('cart');
 Route::get('/category/{alias}', 'CategoryController@showCategoryAlias') -> name('category_name');
 Route::get('/category/{category_id}/{product_id}', 'ProductController@showProduct');
-
 Route::post('/add-to-cart', 'CartController@addToCart')->name('addToCart');
 Route::post('/deleteCart', 'CartController@delete')->name('deleteCart');
 
-Route::get('/clear', function() {
-    Artisan::call('cache:clear');
-    Artisan::call('config:cache');
-    Artisan::call('view:clear');
-    Artisan::call('route:clear');
-    return "Кэш очищен.";
+// Route clear cache (config route)
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('/clear', function() {
+        Artisan::call('cache:clear');
+        Artisan::call('config:cache');
+        Artisan::call('view:clear');
+        Artisan::call('route:clear');
+        Artisan::call('optimize');
+        return "Кэш очищен.";
+    });
 });
 
-
+//Route admin page
+Route::middleware(['role:admin'])->prefix('admin')->group(function () {
+    Route::get('/', 'Admin\HomeController@index');
+    Route::resource('category', Admin\CategoryController::class);
+});
